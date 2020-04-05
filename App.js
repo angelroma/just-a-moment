d402b9cbe24f82219c4a35f6e4f6f45e04188e27
Angel Rodriguez <marodriguez@webcreek.com>
2020-04-04 21:37:17 -0500

    Add button to handle new tasks

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {StatusBar} from 'react-native';
 import Task from './components/Task';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {StyleSheet, Button, View, SafeAreaView, Text, Alert} from 'react-native';
+
+
+const Stack = createStackNavigator();
+
+const buttonStyle = StyleSheet.create({
+  general: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#ee6e73',
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+  },
+});
 
 export default class App extends Component {
+
+
   render() {
     return (
-      <View>
-        <StatusBar hidden={true} />
-        <Task />
-      </View>
+        <NavigationContainer>{
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Task} options={{headerShown: true}}/>
+          </Stack.Navigator>
+        }</NavigationContainer>
     );
   }
 }
